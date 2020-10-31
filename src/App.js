e5a5d01db04803141e4e2e49ded57dba644bdec6
Nero Wei <nero.wei@sscinc.com>
2020-10-30 22:01:02 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
 import logo from './logo.svg';
+import heart from './heart.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const hr_api = 'http://192.168.1.123/status';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      requestFailed: false,
+    }
+  }
+
+  fetchData = () => {
+    fetch(hr_api)
+        .then(response => {
+          if (!response.ok) {
+            throw Error("Request failed")
+          }
+          return response
+        })
+        .then(d => d.json())
+        .then(d => {
+          this.setState(d);
+        }, () => {
+          this.setState({
+            requestFailed: true
+          });
+        })
+  }
+
+  componentDidMount() {
+    this.fetchData();
+    setInterval(this.fetchData, 1000)
+  }
+
+  // TODO: Need to ensure one animation is completed before updating the pulsing speed
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={heart} className="Heart filter-react-blue" alt="Heart"
+               style={ { animation: `heartbeat ${60/this.state.hr}s infinite` } }/>
+          <p>
+            Charging: { this.state.charging ? "True": "False" }
+          </p>
+          <p>
+            Heart Rate: { this.state.hr }
+          </p>
+          <p>
+            Last Updated: { this.state.last_updated }
+          </p>
+          </header>
+        </div>
+    );
+  }
 }
 
 export default App;
